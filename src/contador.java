cc31a6bc3b9a1b2d1adc2f9fc8f8fd1747578d2b
José Carlos Valeriano Rodríguez <78151884+JoseCarlosValeriano@users.noreply.github.com>
2021-05-13 14:00:05 +0100

    Test ValorInicial falla

--- src/contador.java
+++ src/contador.java
@@ -1,13 +1,22 @@
 public class contador {
 
-    public contador(int valorInicial, int valorFinal, int valorIncremento) {
-        this.valorInicial = valorInicial;
-        this.valorFinal = valorFinal;
-        this.valorIncremento = valorIncremento;
-    }
+
+
     int valorInicial = 0;
     int valorFinal = 0;
     int valorIncremento = 0;
 
+
+    public contador(int valorInicial, int valorFinal, int valorIncremento) {
+        this.valorInicial = 0;
+        this.valorIncremento = 1;
+        this.valorFinal = valorFinal;
     
+    }
+    
+    public contador(int valorFinal) {
+        this.valorInicial = 0;
+        this.valorIncremento = 1;
+    }
+
 }
--- src/contadorTest.java
+++ src/contadorTest.java
@@ -4,7 +4,22 @@ import org.junit.Test;
 public class contadorTest {
     @Test
     public void testContador() {
-        contador calculo = new contador(3,4,1);
+        contador calculo = new contador(3, 4, 1);
 
     }
+
+    
+
+    @Test
+    public void valorObligado(int valorInicial, int valorFinal, int valorIncremento) {
+
+        contador Contador = new contador(valorFinal);
+    }
+
+    @Test
+    public void testValorInicial(){
+        contador c = new contador(1);
+        assertEquals(0, c.getValorInicial());
+    }
+
 }
