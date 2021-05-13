c43b5cb502c4a8c1342da594205f8734ded36cc5
José Carlos Valeriano Rodríguez <78151884+JoseCarlosValeriano@users.noreply.github.com>
2021-05-13 13:07:09 +0100

    Solución creación del objeto con los constructores

--- src/contador.java
+++ src/contador.java
@@ -1,3 +1,13 @@
 public class contador {
+
+    public contador(int valorInicial, int valorFinal, int valorIncremento) {
+        this.valorInicial = valorInicial;
+        this.valorFinal = valorFinal;
+        this.valorIncremento = valorIncremento;
+    }
+    int valorInicial = 0;
+    int valorFinal = 0;
+    int valorIncremento = 0;
+
     
 }
