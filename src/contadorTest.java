332b22069269e0d57daff3717f7c768cd3df6737
José Carlos Valeriano Rodríguez <78151884+JoseCarlosValeriano@users.noreply.github.com>
2021-05-13 13:04:15 +0100

    Fallo creacion de objeto

---
+++ src/contadorTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class contadorTest {
+    @Test
+    public void testContador() {
+        contador calculo = new contador(3,4,1);
+
+    }
+}
